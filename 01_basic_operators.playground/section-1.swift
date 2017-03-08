74656cc67d67cd8d310d91675b0c85c2e37face4
JK_MAC <JKSEO@JK-MACui-iMac.local>
2017-03-08 21:54:53 +0900

    Add basic operators

---
+++ 01_basic_operators.playground/section-1.swift
@@ -0,0 +1,29 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//str
+
+// variable 변수
+var number1 = 1
+//number1
+
+// let 상수
+let number2 = 2
+
+// 변수와 상수의 차이 -> 변수는 한번 담은 값을 바꿀 수 있지만 상수는 불가능
+
+1 + 2
+
+13 % 5
+5.0 / 2.0
+5 / 2
+
+//str = 5 문자를 담았던 변수에 숫자 담을 수 없음
+
+"abc" + "edf"
+
+var a = 5
+a += 4
+a
