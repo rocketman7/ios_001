8883daf1245559bba40591a83b701afafdfa3f03
JK_MAC <JKSEO@JK-MACui-iMac.local>
2017-03-12 22:04:34 +0900

    data_types

---
+++ 02_data_types.playground/section-1.swift
@@ -0,0 +1,29 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// implicit: 암시적으로 data type 설정
+var str = "Hello, playground"
+
+//var: 변수, let: 상수(assignment이후 새로 assign 불가)
+
+var number = 1
+
+// explicit: data type을 명시
+var str2:String = "hello"
+//var str3:String = 1 : error
+
+var number2:Int = 2
+
+
+// convert string to integer: 문자형을 정수형으로
+
+var tenstr = "33"
+
+
+var number3 = 5
+"\(number3)"
+
+Int(3.0/2.0)
+
+
