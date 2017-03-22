4c860a6fa8d4320ca3d3ad821c5b699321e9a7b2
Mac <Mac@JK-MacBook-Pro.local>
2017-03-23 00:30:29 +0900

    commit

---
+++ week01_task03.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+
+for i in 0...5 {
+    var rnd = Int(arc4random_uniform(25))
+    var arr = Array(1...25)
+    
+}
